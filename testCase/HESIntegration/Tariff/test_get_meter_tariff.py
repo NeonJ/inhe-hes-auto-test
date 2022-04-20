5877914661d45f4e628191432a3766a888323290
JianNanCao <12345678>
2022-04-20 15:26:43 +0800

    修改费率用例

--- testCase/HESIntegration/Tariff/test_get_meter_tariff.py
+++ testCase/HESIntegration/Tariff/test_get_meter_tariff.py
@@ -15,7 +15,7 @@ class Test_Get_Meter_Tariff:
         print("get request data")
         # active request parameter
         data, user_config = caseData('testData/empower/Tariff/getMeterTariff.json')
-        requestData = data['test_GetMeterTariff_Active']['request']
+        requestData = data['test_GetMeterTariff_Active']
         requestData['meterNo'] = setting[Project.name]['meter_no']
 
         expectResJson = data['response']
@@ -30,10 +30,9 @@ class Test_Get_Meter_Tariff:
         print("get request data")
         # passive request parameter
         data, user_config = caseData('testData/empower/Tariff/getMeterTariff.json')
-        requestData = data['test_GetMeterTariff_Passive']['request']
+        requestData = data['test_GetMeterTariff_Passive']
         requestData['meterNo'] = setting[Project.name]['meter_no']
 
-        expectResJson = data['response']
         response = HESRequest().get(url=url, params=requestData)
         print(response)
 
@@ -46,10 +45,9 @@ class Test_Get_Meter_Tariff:
         print("get request data")
         # request parameter:get all tariff
         data, user_config = caseData('testData/empower/Tariff/getMeterTariff.json')
-        requestData = data['test_GetMeterTariff_All']['request']
+        requestData = data['test_GetMeterTariff_All']
         requestData['meterNo'] = setting[Project.name]['meter_no']
 
-        expectResJson = data['response']
         response = HESRequest().get(url=url, params=requestData)
         print(response)
 
