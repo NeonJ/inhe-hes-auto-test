248500a6b9f4982283c7242f924db09eaa18d6d8
JianNanCao <12345678>
2022-05-07 18:01:13 +0800

    修改启动方法

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ parser.add_argument("--path", help="report  path", default='/')
 parser.add_argument("--resume", help="continue last obis check", default='False', choices=['False', 'True'])
 parser.add_argument("--retry", help='failed retries', default='0')
 parser.add_argument("--group", help='nacos group', default='QA', choices=['QA', 'DEV'])
+parser.add_argument("--tester", help='tester name', default=socket.gethostname())
 
 args = parser.parse_args()
 
@@ -43,6 +44,7 @@ def config():
     config_dict['resume'] = args.resume
     config_dict['retry'] = args.retry
     config_dict['group'] = args.group
+    config_dict['tester'] = args.tester
     return config_dict
 
 
@@ -55,7 +57,7 @@ def writeConfig():
         yaml.dump(data=config(), stream=f, allow_unicode=True)
 
 
-var = '--project {} --tag {} --path {} --resume {} --retry {} --group {}'.format(args.project, args.tag, args.path,
+var = '--project {} --tag {} --tester {} --resume {} --retry {} --group {}'.format(args.project, args.tag, args.tester,
                                                                                  args.resume, args.retry, args.group)
 print(var)
 writeConfig()
@@ -84,21 +86,19 @@ if os.listdir(result_path) != []:
     all_data, reportUrl = update_trend_data(buildOrder, old_data)
     if not os.path.exists('{}/report_history'.format(report_path)):
         os.mkdir('{}/report_history'.format(report_path))
-    print(report_path)
     shutil.copytree('{}/{}/{}'.format(report_path, args.project, buildOrder),
                     '{}/report_history/{}'.format(report_path, buildOrder))
     report_date = time.strftime('%y%m%d%H%M%S', time.localtime())
     history_path = '{}/report_history/{}'.format(report_path, args.project + '-' + report_date)
-    print(history_path)
     os.rename('{}/report_history/{}'.format(report_path, buildOrder),
-              '{}/report_history/{}'.format(report_path, args.project + '-' + socket.gethostname() + '-' + report_date))
+              '{}/report_history/{}'.format(report_path, args.project + '-' + args.tester + '-' + report_date))
     # 推送报告到报告仓库
     warehouse_dir = r'/opt/tomcat/webapps'
     local_report_dir = r'{}/report_history/{}'.format(report_path,
-                                                      args.project + '-' + socket.gethostname() + '-' + report_date)
+                                                      args.project + '-' + args.tester + '-' + report_date)
     host = Linux('10.32.233.164', 'root', 'kaifa123')
     host.sftp_put_dir(local_report_dir, warehouse_dir)
     print('Report URL == http://10.32.233.164:9090/{}/'.format(
-        args.project + '-' + socket.gethostname() + '-' + report_date))
+        args.project + '-' + args.tester + '-' + report_date))
 else:
     print('无结果数据，无法生成报告')
